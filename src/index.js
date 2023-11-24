c1d68bd28290ecfd6ac23c49a8e447bf831b5ae0
educalo <64586573+educalo@users.noreply.github.com>
2023-11-24 21:17:45 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,27 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-//4.- Podemos simplicarlo utilizando desectructuracion
-//si utilizo esta forma es exactamente lo mismo que lo comentado dos lineas anteriores
-
-//function Componente ({titulo, contenido}) para desectructurar directamente en el paso de la props, si es asi sobraria la linea const {titulo, contenido}= props
-
-function Componente(props){
+//5.- Si queremos mejorar el codigo del punto 4 podriamos hacer lo siguiente
+//children son los hijos de mi componente
+function Componente({ titulo, children }){
   //contiene un objeto, que tiene como atributos contenido y titulo
-  console.log(props);
+  //console.log(props);
     //const titulo = props.titulo;
     //const contenido =props.contenido;
-    const {titulo, contenido} = props;
+    //const {titulo, contenido} = props;
     
     return ( <div>
     <h1>{titulo}</h1>
-    <div>{contenido}</div>
+    <div>{children}</div>
     </div>
   );
   }
-
   //ahora para utilizar los componentes lo pongo de la siguiente manera
   //al componente podemos pasarle atributos, en nuestro ejemplo seria Titulo y Contenido
   ReactDOM.render(
-    <Componente titulo="Titulo" contenido="Contenido" />, document.getElementById("root")
+    <Componente titulo="Titulo1">Hola desde React</Componente>, document.getElementById("root")
     );
