bf014742452a8c6231e65c058d2a77e52afe3bac
educalo <64586573+educalo@users.noreply.github.com>
2023-11-24 21:37:11 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-//elemento creado en la renderización
-const contenido =
-  <div>
-    <h1>Titulo</h1>
-    <div>Hola desde React</div>
-  </div>;
-  
-ReactDOM.render(
-  contenido, document.getElementById("root")
-  );
-  
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
