712dc2ffb809055360343053f41d665fb6604794
educalo <64586573+educalo@users.noreply.github.com>
2023-11-24 19:34:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//elemento creado en la renderización
+const contenido =
+  <div>
+    <h1>Titulo</h1>
+    <div>Hola desde React</div>
+  </div>;
+  
+ReactDOM.render(
+  contenido, document.getElementById("root")
+  );
+  
