75fccd30e4e76899444ea3bdca16fcbedeb70780
educalo <64586573+educalo@users.noreply.github.com>
2023-11-24 21:10:30 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,19 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+//3.- Añado props para pasarle datos al componente
+//paso las propiedades como expresiones props.Titulo y props.Contenido
 
-  function Componente(){
-    return ( <div>
-    <h1>Titulo</h1>
-    <div>Hola desde React</div>
-    </div>
-  );
-  }
-  
-  //ahora para utilizar los componentes lo pongo de la siguiente manera
-  //al componente podemos pasarle atributos, en nuestro ejemplo seria Titulo y Contenido
-  ReactDOM.render(
-    <Componente />, document.getElementById("root")
+function Componente(props){
+    //contiene un objeto, que tiene como atributos contenido y titulo
+    console.log(props);
+    const titulo = props.titulo;
+    const contenido =props.contenido;
+      return ( <div>
+      
+      <h1>{titulo}</h1>
+      <div>{contenido}</div>
+      </div>
     );
-  
+    }
+    //ahora para utilizar los componentes lo pongo de la siguiente manera
+    //al componente podemos pasarle atributos, en nuestro ejemplo seria Titulo y Contenido
+    ReactDOM.render(
+      <Componente titulo="Titulo" contenido="Contenido" />, document.getElementById("root")
+    );
 
